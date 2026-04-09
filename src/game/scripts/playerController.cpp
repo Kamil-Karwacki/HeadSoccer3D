373d9378fb2bbc70086784dbf331b12af9361b45
Kamil <kamil@fedora.home>
2026-04-09 23:41:16 +0200

    Fixed bug where player wasn't able to rotate the footballer

--- src/game/scripts/playerController.cpp
+++ src/game/scripts/playerController.cpp
@@ -20,43 +20,6 @@ void PlayerController::onStart()
 
 void PlayerController::onUpdate(float deltaTime)
 {
-    /*
-    Transform* transform = m_entity->GetComponent<Transform>();
-    Rigidbody* rigidbody = m_entity->GetComponent<Rigidbody>();
-
-    if (!transform || !rigidbody) return;
-
-    static constexpr float speed = 2000.0f;
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-
-    Application& app = Application::Get();
-    InputManager manager = app.GetInput();
-
-    glm::vec3 front = transform->getFront();
-    if (manager.isActionHeld("forward"))
-    {
-        rigidbody->m_forceAcc += -front * deltaTime * speed;
-    }
-    if (manager.isActionHeld("back"))
-    {
-        rigidbody->m_forceAcc += front * deltaTime * speed;
-    }
-    if (manager.isActionHeld("left"))
-    {
-        rigidbody->m_forceAcc += glm::cross(front, up) * deltaTime * speed;
-    }
-    if (manager.isActionHeld("right"))
-    {
-        rigidbody->m_forceAcc += -glm::cross(front, up) * deltaTime * speed;
-    }
-
-    float mouseDeltaX = static_cast<float>(manager.getMouseDeltaX());
-
-    float mouseSensitivity = 0.001f;
-    m_yaw += mouseDeltaX * mouseSensitivity;
-
-    transform->setRotation(glm::vec3(0.0f, -m_yaw, 0.0f));
-*/
     Footballer* footballer = m_entity->GetComponent<Footballer>();
     if (!footballer) return;
 
@@ -83,4 +46,11 @@ void PlayerController::onUpdate(float deltaTime)
     {
         footballer->kickBall();
     }
+
+    float mouseDeltaX = static_cast<float>(manager.getMouseDeltaX());
+
+    float mouseSensitivity = 0.001f;
+    m_yaw += mouseDeltaX * mouseSensitivity;
+
+    footballer->m_angularInput = m_yaw;
 }
